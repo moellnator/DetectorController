3f379849699d78029a4663286cd6b0ceab355752
c-hahn <christoph.hahn@uni-jena.de>
2017-05-24 08:17:23 +0200

    Comments on implementation
    Expanded some comments on implementation difficulties

--- rp_auto_smswarning.py
+++ rp_auto_smswarning.py
@@ -8,7 +8,7 @@ class SmsWarning:
     
     def __init__( self, name, modem, recipients, suppress ):
     
-        self.logger = logging.getLogger('rp_auto_ctrl')   # tie into the global rp_auto_ctrl logger -- \ch: unfortunately this means that logging messages have 'SmsWarning' as module descriptor
+        self.logger = logging.getLogger('rp_auto_ctrl')   # tie into the global rp_auto_ctrl logger. \ch: unfortunately this means that logging messages have 'SmsWarning' as module descriptor. Attempting to overwrite %(module) results in an error. Could be circumvented with lr=logging.makeLogRecord(), self.logger.handle(lr) --- but should we?
         self.name = name          # the identifier
         self.suppress = suppress  # the suppression time of the warning, in seconds
         self.modem = modem        # the sms modem
@@ -16,7 +16,7 @@ class SmsWarning:
 
     def Send( self, message ):
 
-        self.logger.warning('WARN[' + name + ']: ' + message)
+        self.logger.warning('WARN[' + name + ']: ' + message)   # \ch: might be better to separate logger() handling of warning messages from this module's sms sending
         if not self.first_issued > 0:
             self.first_issued=datetime.datetime.now()
         if (datetime.datetime.now() - self.last_issued).total_seconds() >= self.suppress :
