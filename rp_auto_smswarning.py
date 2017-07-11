f8c2de8a96c81ba42a8c544d948c402ed87f5134
c-hahn <christoph.hahn@uni-jena.de>
2017-07-11 13:42:09 +0200

    Name fix in rp_auto_smswarning
    Fixed naming error that prevented issue resolution (probably) (maybe)

--- rp_auto_smswarning.py
+++ rp_auto_smswarning.py
@@ -29,7 +29,7 @@ class SmsWarning:
     
         while True:
             if self.IsIssued(): # warning is marked as "active"...
-                if (date.datetime.now() - self.last_issued).total_seconds() >= self.release:    # ... but has not been encountered for self.release seconds
+                if (datetime.datetime.now() - self.last_issued).total_seconds() >= self.release:    # ... but has not been encountered for self.release seconds
                     self.Resolve()
             time.sleep(10)  # \ch: need some time here to avoid hogging resources, but not too long w.r.t. self.release
 
